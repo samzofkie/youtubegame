1ecc6cdcb5f949484879039cac07aaff7f816cbc
samzofkie <samzofkie@gmail.com>
2021-02-23 09:03:13 -0500

    Added recognition of AWS env vars 4 connectin to database

--- config.py
+++ config.py
@@ -15,10 +15,18 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     VIDEO_REPLACEMENT = True
-    CRAWL = True
+    CRAWL = False
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    DEBUG = False
+    if 'RDS_HOSTNAME' in os.environ:
+        USERNAME = os.environ['RDS_USERNAME']
+        PASSWORD = os.environ['RDS_PASSWORD']
+        ENDPOINT = os.environ['RDS_HOSTNAME']
+        DB_NAME = os.environ['RDS_DB_NAME']
+        SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{ENDPOINT}/{DB_NAME}'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     VIDEO_REPLACEMENT = False
     CRAWL = True
